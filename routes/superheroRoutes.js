4320ec9ec54aa4df26ff87042b8434253aa7ed08
danibarker <caseybarker86@gmail.com>
2022-04-13 12:08:33 -0600

    added post endpoint for creating superhero

--- routes/superheroRoutes.js
+++ routes/superheroRoutes.js
@@ -1,14 +1,32 @@
 var express = require("express");
 var router = express.Router();
 var debug = require("debug")("server:routes");
-const { getAllSuperheroes } = require("../db/models/superheroModel");
+const {
+  getAllSuperheroes,
+  createSuperhero,
+} = require("../db/models/superheroModel");
 
-router.get("/", async (req, res, next) => {
+router.get("/", async (req, res) => {
   try {
     debug("getting all superheroes");
     const superheroes = await getAllSuperheroes();
     res.send(superheroes);
   } catch (err) {
+    debug(err.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const newSuperhero = req.body;
+  debug(`adding new superhero: ${newSuperhero.name}`);
+  try {
+    const addedSuperhero = await createSuperhero(newSuperhero);
+    debug(
+      `added new superhero: ${addedSuperhero.name} with _id ${addedSuperhero._id}`
+    );
+    res.send(addedSuperhero);
+  } catch (err) {
+    debug(`failed to add new superhero: ${newSuperhero.name}`);
     debug(err.message);
   }
 });
