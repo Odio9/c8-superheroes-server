ec54f2b151e62bb4f59c553778a82a37fc44e99a
Zach Odio <zachariaodio@gmail.com>
2022-05-11 11:45:15 -0600

    add nes testing file

---
+++ __test__/userModel.test.js
@@ -0,0 +1,20 @@
+describe('User model', () => {
+    it('should find a created user in the list of users', () => {
+
+    })
+    it('should find a created user in the list of users', () => {
+        
+    })
+
+    it('should find a created user in the list of users', () => {
+        
+    })
+
+    it('should find a created user in the list of users', () => {
+        
+    })
+
+    it('should find a created user in the list of users', () => {
+        
+    })
+});
