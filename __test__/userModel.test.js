bddaf79bc96beb70eb9359efd639eb97f7f1dd51
Zach Odio <zachariaodio@gmail.com>
2022-05-11 12:32:12 -0600

    changes in usermodeltest

--- __test__/userModel.test.js
+++ __test__/userModel.test.js
@@ -1,21 +1,81 @@
-describe("User model", () => {
-  it("should find a created user in the list of users", () => {
-    throw new Error("unimplemented");
-  })
-  it.skip("should find a created user by id", () => {
-    throw new Error("unimplemented");
-  })
+const {
+    deleteAllUsers,
+    createUser,
+    getUserByUsername,
+    getUserById,
+    updateUser,
+    verifyPassword,
+} = require('../db/models/userModel')
 
-  it("should find a created user in the list of users", () => {
-    throw new Error("unimplemented");
-  })
+describe('User model', () => {
 
-  it("should find a created user in the list of users", () => {
-    throw new Error("unimplemented");
-  })
+    beforeEach(async () => {
+        await deleteAllUsers()
+    })
 
-  it("should find a created user in the list of users", () => {
-    throw new Error("unimplemented"); 
-  })
+    it('should find a created user by username', async () => {
+        // setup
+        await createUser({ username: 'testUser', password: 'testPassword'})
+
+        // execute
+        let foundUser = await getUserByUsername('testUser') 
+
+        // verify
+        expect(foundUser.username).toBe('testUser')
+    })
+
+    it('should default new users to not agent', async () => {
+        // setup
+
+        // execute
+        await createUser({ username: 'testUser', password: 'testPassword'})
+
+        // verify
+        let foundUser = await getUserByUsername('testUser') 
+        expect(foundUser.isAgent).toBe(false)
+    })
+
+    it('should encrypt user passwords', async () => {
+        // setup
+
+        // execute
+        await createUser({ username: 'testUser', password: 'testPassword'})
+
+        // verify
+        let foundUser = await getUserByUsername('testUser') 
+        expect(foundUser.password).not.toBe('testPassword')
+    })
+
+    it('should verify a password for a user', async () => {
+        // setup
+        await createUser({ username: 'testUser', password: 'testPassword'})
+        let foundUser = await getUserByUsername('testUser') 
+
+        // execute
+        let verified = await verifyPassword('testPassword', foundUser.password)
+
+        // verify
+        expect(verified).toBe(true)
+    })
+
+    it('should verify a password for a user doesnt match', async () => {
+        // setup
+        await createUser({ username: 'testUser', password: 'testPassword'})
+        let foundUser = await getUserByUsername('testUser') 
+
+        // execute
+        let verified = await verifyPassword('someOtherPassword', foundUser.password)
+
+        // verify
+        expect(verified).toBe(false)
+    })
+
+    it.skip('should not allow a repeated username', async () => {
+        throw new Error('unimplemented')
+    })
+
+    it.skip('should find a created user by id', async () => {
+        throw new Error('unimplemented')
+    })
 
 });
